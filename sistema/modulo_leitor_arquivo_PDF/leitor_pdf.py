d274daf0b0789b93c0160981cac8e2e90931eff8
Pedro Lucas França <73343509+fpedrolucas95@users.noreply.github.com>
2023-07-14 15:58:23 -0300

    Melhorias para ser mais rápido

--- sistema/modulo_leitor_arquivo_PDF/leitor_pdf.py
+++ sistema/modulo_leitor_arquivo_PDF/leitor_pdf.py
@@ -1,27 +1,27 @@
 import os
 import torch
-from flask_socketio import emit
 from torchvision import models, transforms
 from PIL import Image
 from PyPDF2 import PdfReader
 import pdf2image
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
-# Configurações
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_path = "modelo/modelo_guia_SADT.pth"
 temp_folder = "/tmp/"
 os.makedirs(temp_folder, exist_ok=True)
 
-# Carregar modelo
-model = models.resnet50(weights=None)  # Mudança aqui
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 2)
-model.load_state_dict(torch.load(model_path, map_location=device))
-model = model.to(device)
-model.eval()
+# A função 'load_model' continua sendo usada da mesma forma que no código anterior, para carregar o modelo apenas quando necessário.
+def load_model(model_path, device):
+    model = models.resnet50(weights=None)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model = model.to(device)
+    model.eval()
+    return model
 
-# Função de transformação
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -29,31 +29,42 @@ transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
-# Função para analisar PDF e identificar guias SADT
+# Aqui foi criada uma nova classe, 'PDFImagesDataset', para representar as imagens no PDF como um conjunto de dados do PyTorch. Isso é necessário para usar o DataLoader do PyTorch.
+class PDFImagesDataset(Dataset):
+    def __init__(self, pdf_path, dpi=300, transform=None):
+        self.pdf_path = pdf_path
+        self.dpi = dpi
+        self.transform = transform
+        self.pdf_file = PdfReader(pdf_path)
+        self.num_pages = len(self.pdf_file.pages)
+
+    def __len__(self):
+        return self.num_pages
+
+    def __getitem__(self, idx):
+        image = pdf2image.convert_from_path(self.pdf_path, dpi=self.dpi, first_page=idx+1, last_page=idx+1, fmt='png')[0]
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+# Na função 'analisar_pdf', as imagens são carregadas em lotes usando o DataLoader do PyTorch, e então o modelo é aplicado a cada lote, em vez de a cada imagem individualmente. 
+# Isso permite que a GPU processe várias imagens ao mesmo tempo, melhorando a velocidade do processamento.
 def analisar_pdf(pdf_path, callback):
-    pdf_file = PdfReader(pdf_path)
-    num_pages = len(pdf_file.pages)
+    model = load_model(model_path, device)
+    dataset = PDFImagesDataset(pdf_path, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=32, num_workers=4)
+
     guias_sadt_pages = []
-
-    for i in range(num_pages):
-        # Calcular e imprimir a porcentagem de conclusão
-        completion_percentage = (i / num_pages) * 100
-        callback(completion_percentage)
-
-        image_path = f"{temp_folder}{i}.png"
-        image = pdf2image.convert_from_path(
-            pdf_path, dpi=300, first_page=i+1, last_page=i+1, fmt='png')[0]
-        image.save(image_path, "PNG")
-
-        image = Image.open(image_path).convert("RGB")
-        image = transform(image).unsqueeze(0).to(device)
-
-        outputs = model(image)
+    for i, batch in enumerate(dataloader):
+        batch = batch.to(device)
+        outputs = model(batch)
         _, predicted = torch.max(outputs.data, 1)
-
-        if predicted.item() == 0:
-            guias_sadt_pages.append(i+1)
-
-        os.remove(image_path)
-
+        
+        # O cálculo do percentual de conclusão e a verificação das previsões são feitos da mesma forma que antes, mas agora por lote em vez de por imagem.
+        for j, prediction in enumerate(predicted):
+            page_num = i * dataloader.batch_size + j + 1
+            completion_percentage = page_num / len(dataset) * 100
+            callback(completion_percentage)
+            if prediction.item() == 0:
+                guias_sadt_pages.append(page_num)
     return guias_sadt_pages
